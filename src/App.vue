f6d977441c224444bac437eb6d14cd2217a8fda4
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-12-28 13:34:13 +0100

    5

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,33 @@
+<template>
+	<div class="app">
+		<button @click="handleClick('title')">order by title</button>
+		<button @click="handleClick('salary')">order by salary</button>
+		<button @click="handleClick('location')">order by location</button>
+
+		<JobList :jobs="jobs" :order="order" />
+	</div>
+</template>
+
 <script setup lang="ts">
-import { reactive, toRefs, ref } from 'vue'
+import { ref } from 'vue'
 import Job from './types/Job'
+import OrderTerm from './types/OrderTerm'
+
+import JobList from './components/JobList.vue'
 
 const jobs = ref<Job[]>([
-	{ title: 123, location: 'ranch', salary: 44654, id: '1' },
+	{ title: 'farmer', location: 'ranch', salary: 44654, id: '1' },
 	{ title: 'quarryman', location: 'mounatain', salary: 12313, id: '2' },
 	{ title: 'flute player', location: 'wood', salary: 34535, id: '3' },
 	{ title: 'fisherman', location: 'lake', salary: 75456, id: '4' },
 	{ title: 'guard', location: 'valley', salary: 23452, id: '5' },
 ])
 
-console.log(typeof jobs.value[0]?.title)
-</script>
+const order = ref<OrderTerm>('title')
 
-<template>
-	<div class="app">{{ jobs[0].location }}</div>
-</template>
+const handleClick = (term: OrderTerm) => {
+	order.value = term
+}
+</script>
 
 <style scoped></style>
---
+++ src/components/JobList.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="job-list">
+		<p>Ordered by {{ order }}</p>
+		<ul>
+			<li class="card" v-for="job in jobs" :key="job.id">
+				<h2>{{ job.title }} in {{ job.location }}</h2>
+				<div class="salary">
+					<p>{{ job.salary }} dollars</p>
+				</div>
+				<div class="description">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit
+						tenetur nihil saepe perspiciatis sint provident alias nulla, enim
+						laudantium unde.
+					</p>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { PropType, defineProps } from 'vue'
+import Job from '../types/Job'
+import OrderTerm from '../types/OrderTerm'
+
+defineProps({
+	jobs: { type: Array as PropType<Job[]>, required: true },
+	order: { type: String as PropType<OrderTerm>, required: true },
+})
+</script>
+
+<style scoped>
+.card {
+	background: rgb(210, 200, 63);
+}
+</style>
